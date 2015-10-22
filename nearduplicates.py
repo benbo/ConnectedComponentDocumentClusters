b8925a835e700245bbdf8b60c8e0d6cf3b27b3f6
Benedikt Boecking <benediktb@lov3.int.autonlab.org>
2015-10-22 10:53:30 -0400

    added more tests, changed the hashing slightly.

--- nearduplicates.py
+++ nearduplicates.py
@@ -11,12 +11,13 @@ from datasketch import MinHash
 import random, struct
 from random import sample,choice
 from sklearn import metrics
-
+from zlib import crc32
 NUM_PERM=100
 
-#use 32-bit hashing
-M_PRIME = (1 << 61) - 1
-MAX_HASH = (1 << 32) - 1
+
+#we truncate sha1 for now. We should probably replace this with a proper hash function.
+M_PRIME = (1 << 89) - 1 #(x << n) is x shifted left by n bit
+MAX_HASH = (1 << 64) - 1 
 
 random.seed(427)
 A,B = np.array([(random.randint(1, M_PRIME),random.randint(0, M_PRIME)) for _ in range(NUM_PERM)]).T
@@ -27,8 +28,9 @@ A,B = np.array([(random.randint(1, M_PRIME),random.randint(0, M_PRIME)) for _ in
 
 def get_permuted_hashes(token):
     # get a hash value
-    hv = struct.unpack('<I', sha1(token).digest()[:4])[0]
-    #do random bijective permutation
+    #abusing sha1 and truncating to 12 digit number
+    hv=int(sha1(token).hexdigest(),16)% (10 ** 12) 
+    #do Carter and Wegman like hashing.
     return np.bitwise_and((A * hv + B) % M_PRIME,MAX_HASH)
 
 def get_lsh(sig,nbands):
@@ -118,6 +120,7 @@ def run_near_duplicates(obj):
     '''
     get near duplicates for a seed ad
     currently requires as input
+        - hashcorp: dictionary of documentID:minhash
         - doc2lsh: dictionary of documentID:lsh_signatures 
         - lshdict: dictionary of lsh_signature:documentIDs
         - seed: seed document id
@@ -126,12 +129,13 @@ def run_near_duplicates(obj):
         -set of documentIDs
     '''
     seed = obj['seed']
-    lshdict = obj['lshdict']
-    doc2lsh = obj['doc2lsh']
+    hashcorp = obj['hashcorp']
+    lshdict = obj['lsh_dict']
+    doc2lsh = obj['doc_to_lsh']
     t= obj['threshold']
     cluster=set([seed])
     #get candidates and flatten list
-    candidates=set(itertools.chain.from_iterable([lshdict[sig] for sig in doc2lsh[s]]))
+    candidates=set(itertools.chain.from_iterable([lshdict[sig] for sig in doc2lsh[seed]]))
     m1=hashcorp[seed]
     for cand in candidates:
         if cand in cluster:continue#don't check if we've already added this
@@ -141,48 +145,50 @@ def run_near_duplicates(obj):
     #all candidates have been checked 
     return cluster 
 
-def run_getminhash(obj):
+def run_getminhash(node):
     '''
     Compute minhash signatures for raw text.
-    This functions takes documents and their ids as input and returns ids and the corresponding minhash signature.
+    This functions takes a document and documentID as input and returns, the documentID and the corresponding minhash signature.
     The minhash signature is a set of NUM_PERM positiv integer values created with NUM_PERM hash functions.
     The minhash signature allows for a fast Jaccard similariy estimation of two documents. 
     input:
-        keys,documents (TODO define exact input. Dicionary or json.)
+        node: dictionary with id,text
     output:
-        dictionary of key,hashvalues (this can easily be modified to return a list of tuples or a json object) 
+        node: dicionary with id,hashvalues (id,hashv)
     '''
     #compute hashes
-    hashcorp=dict.fromkeys([key for key,doc in obj['objects']])
-    for key,doc in obj['objects']:
-        #compute minhash signature
-        hashvalues=np.empty(NUM_PERM)
-        hashvalues.fill(MAX_HASH)
-        for token in doc.lower().split(): 
-            hashvalues = np.minimum(get_permuted_hashes(token.encode('utf-8','ignore')), hashvalues)
-        #do something with the hash. save it? return it?
-        hashcorp[key]=hashvalues
-    return hashcorp
+    output_node={
+            'id'    :   node['id'],
+            'hashv' :   None
+            }
+    #compute minhash signature
+    hashvalues=np.empty(NUM_PERM)
+    hashvalues.fill(MAX_HASH)
+    for token in node['text'].lower().split(): 
+        hashvalues = np.minimum(get_permuted_hashes(token.encode('utf-8','ignore')), hashvalues)
+    output_node['hashv']=hashvalues
+    return output_node
 
-def run_lsh(obj):
+def run_lsh_batch(obj):
     """
     Compute the Locality sensitive hashing signatures for a particular threshold.
     Utilize the fact that similar items generate similar hashcodes. 
     LSH signatures help to get a small set of candidates to which a document needs to be compared.
     To get an accurate estimate of Jaccard similarity, the similariy still needs to be computed using the minhash integer signature. 
     input:
-        threshold (default 0.9)
-        key,hashvalues
+        obj with threshold (default 0.9),id,hashvalues
     """
     if 'threshold' in obj:
         thr=obj['threshold']
     else:
-        thr=0.9
+        thr=0.8
     bandwidth=get_bandwidth(NUM_PERM, thr)#r
     bands=int(math.ceil(float(NUM_PERM)/float(bandwidth)))#b
     doc_to_lsh={}
     lsh_dict={}
-    for key,hashvalues in obj['hashcorp']:
+    for node in obj['data']:
+        key=node['id']
+        hashvalues=node['hashv']
         #compute lsh 
         signatures = [sig for sig in get_lsh(hashvalues,bands)]
         #store signatures for this document
@@ -194,6 +200,35 @@ def run_lsh(obj):
             else:
                 lsh_dict[sig]=[key]
     return doc_to_lsh,lsh_dict
+
+
+def run_lsh(obj):
+    """
+    Compute the Locality sensitive hashing signatures for a particular threshold.
+    Utilize the fact that similar items generate similar hashcodes. 
+    LSH signatures help to get a small set of candidates to which a document needs to be compared.
+    To get an accurate estimate of Jaccard similarity, the similariy still needs to be computed using the minhash integer signature. 
+    input:
+        obj with threshold (default 0.9),id,hashvalues
+    """
+    if 'threshold' in obj:
+        thr=obj['threshold']
+    else:
+        thr=0.8
+    bandwidth=get_bandwidth(NUM_PERM, thr)#r
+    bands=int(math.ceil(float(NUM_PERM)/float(bandwidth)))#b
+    doc_to_lsh={}
+    lsh_dict={}
+    key,hashvalues = obj['data']['id'],obj['data']['hashv']
+    #compute lsh 
+    signatures = [sig for sig in get_lsh(hashvalues,bands)]
+    #store signatures for this document
+    doc_to_lsh[key]=signatures
+    #store lsh signature to key
+    for sig in signatures:
+        lsh_dict[sig]=[key]
+    return doc_to_lsh,lsh_dict
+
 
 
 
--- test_nearduplicates.py
+++ test_nearduplicates.py
@@ -1,35 +1,52 @@
 import nearduplicates
 from hashlib import sha1
-from datasketch import MinHash
 
 corpus=[
-        'Vice President Biden announced that he won\'t enter the race for the 2016 presidential nomination',
-        'Vice President Joe Biden said that he will no enter the race for the 2016 presidential nomination',
-        'When 2012 GOP nominee Mitt Romney picked Ryan to be his vice presidential pick, the choice was largely seen as a risky one',
-        'When 2012 republican nominee Romney picked Ryan to be his vice presidential pick, it was largely seen as a risky choice'
+        'Vice President Biden announced that he will not enter the race for the 2016 presidential nomination',
+        'Vice President Joe Biden said that he will not enter the race for the 2016 presidential nomination',
+        'This is exactly the probability of collision we would expect if the hash function assigned truly random hash codes to every key',
+        'This is exactly the probability of collision one would expect if our hash function assigned a truly random hash to every key'
         ]
 
-def test():
-    hashcorp=nearduplicates.run_getminhash({'objects':[(i,x) for i,x in enumerate(corpus)]})
-
-    print "actual_jaccard "
+def test_approximation():
+    hashcorpus = [nearduplicates.run_getminhash({'id':i,'text':x}) for i,x in enumerate(corpus)]
+    #create a dictionary from the nodes
+    hashdict={obj['id']:obj['hashv'] for obj in hashcorpus}
     for i in xrange(4):
         for j in xrange(i+1,4):
             print i,j
-            s1 = set(corpus[i])
-            s2 = set(corpus[j])
+            s1 = set(corpus[i].split())
+            s2 = set(corpus[j].split())
             print "jaccard    : " + str( float(len(s1.intersection(s2)))/float(len(s1.union(s2))))
-            a1= hashcorp[i]
-            a2= hashcorp[j]
+            a1= hashdict[i]
+            a2= hashdict[j]
             print "minhash jac: " + str(nearduplicates.run_jaccard_array({'signatures':(a1,a2)}))
-            m1, m2 = MinHash(), MinHash()
-            for d in corpus[i].split():
-                m1.digest(sha1(d.encode('utf8')))
-            for d in corpus[j].split():
-                m2.digest(sha1(d.encode('utf8')))
-            print("Estimated Jaccard for data1 and data2 is", m1.jaccard(m2))
+
+def test():
+    #create minhash signature for all documents
+    hashcorpus = [nearduplicates.run_getminhash({'id':i,'text':x}) for i,x in enumerate(corpus)]
+    #get lsh buckets for threshold 0.7.
+    #when searching for near duplicates, always make sure that the lsh signatures were created for the same threshold
+    doc_to_lsh,lsh_dict = nearduplicates.run_lsh_batch({'threshold':0.7,'data':hashcorpus})
+
+    #create a dictionary from the nodes so we can look up the minhash for each document id
+    hashdict={obj['id']:obj['hashv'] for obj in hashcorpus}
+    #find near duplicates for each text in the corpus
+    for i in xrange(4):
+        print 'near duplicates for: \t'+corpus[i]
+        #Find near duplicates. 
+        #The returned cluster is a set of ids.
+        #The returned cluster currently also contains the seed itself.
+        cluster = nearduplicates.run_near_duplicates({'seed':i,'hashcorp':hashdict,'doc_to_lsh':doc_to_lsh,'lsh_dict':lsh_dict,'threshold':0.7})
+        for j in cluster:
+            if j!=i:
+                print '\t'+corpus[j]
+
 
 
 
 if __name__ == "__main__":
+    print "testing minhash's jaccard appoximation for short documents"
+    test_approximation()
+    print "test workflow for finding near duplicates"
     test()
